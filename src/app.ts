832c5eeee153f1ad56e1d90cfef13ba3be71eec6
Julio <julioaranabr@yahoo.com.br>
2022-02-22 18:31:56 -0300

    route post user

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
 import express from 'express';
+import createUser from './controllers/user';
+import validatClasse from './middlewares/validateClasse';
+import validatLevel from './middlewares/validateLevel';
+import validateName from './middlewares/validateName';
+import validatePassword from './middlewares/validatePassword';
 
 const app = express();
 
 app.use(express.json());
+app.post(
+  '/users', 
+  validateName,
+  validatClasse,
+  validatLevel,
+  validatePassword,
+  createUser,
+);
 
 export default app;
