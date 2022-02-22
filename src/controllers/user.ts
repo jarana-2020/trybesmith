32ded337291065478e6ce26eae8c8e4aa849f1cd
Julio <julioaranabr@yahoo.com.br>
2022-02-22 18:30:58 -0300

    user controller

---
+++ src/controllers/user.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { Secret, sign } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import createNewUser from '../services/user';
+
+dotenv.config();
+
+const key: Secret = process.env.JWT_SECRET || 'julio';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const dataUser = req.body;
+    const user = await createNewUser(dataUser);
+    const token = sign({ id: user.id, userName: user.username }, key);
+    return res.status(201).json({ token });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export default createUser;
