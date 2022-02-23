2bc326b7a84f94dabae637257bc1ad2f16239466
Julio <julioaranabr@yahoo.com.br>
2022-02-23 18:51:48 -0300

    refatored login controller

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -3,6 +3,7 @@ import { Secret, sign } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 // import { HttpExceptions } from '../middlewares/error';
 import loginUser, { ErrorMessage } from '../services/login';
+import { User } from '../models/user';
 
 dotenv.config();
 
@@ -11,15 +12,16 @@ const key: Secret = process.env.JWT_SECRET || 'julio';
 const executeLogin = async (req: Request, res: Response, _next: NextFunction) => {
   try {
     const { username } = req.body;
-    const user = await loginUser(req.body);
-    const userMessage = user as ErrorMessage;
+    const data = await loginUser(req.body);
+    const user = data as User;
+    const userMessage = data as ErrorMessage;
     if (userMessage.message) {
       return res.status(userMessage.code).json(userMessage.message);
     }
     // if (!user) {
     //   return next(new HttpExceptions(401, 'Username or password invalid'));
     // }
-    const token = sign({ user: username }, key);
+    const token = sign({ user: username, id: user.id }, key);
     return res.status(200).json({ token });
   } catch (error) {
     res.status(500).json(error);
