f9ec1186bbc506276e899177f53210f98a20d3e2
Julio <julioaranabr@yahoo.com.br>
2022-02-23 18:48:33 -0300

    refatored product controller

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
+import { PersonalRequest } from '../middlewares/validateToken';
 import createNewProduct, { allProducts } from '../services/product';
 
-const createProduct = async (req: Request, res: Response) => {
+const createProduct = async (req: PersonalRequest, res: Response) => {
   try {
     const product = await createNewProduct(req.body);
     const { id, name, amount } = product;
