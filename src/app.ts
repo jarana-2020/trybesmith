82854bd8e41504d647994b56f4b5331e41e3cfef
Julio <julioaranabr@yahoo.com.br>
2022-02-23 11:38:19 -0300

    route post product

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import executeLogin from './controllers/login';
+import createProduct from './controllers/product';
 import createUser from './controllers/user';
+import errorMidleware from './middlewares/error';
 import validatClasse from './middlewares/validateClasse';
 import validatLevel from './middlewares/validateLevel';
 import validateName from './middlewares/validateName';
 import validatePassword from './middlewares/validatePassword';
+import checkNameProduct, { checkAmountProduct } from './middlewares/validateProduct';
+import validateToken from './middlewares/validateToken';
 
 const app = express();
 
@@ -18,5 +22,13 @@ app.post(
   createUser,
 );
 app.post('/login', validateName, validatePassword, executeLogin);
+app.post(
+  '/products', 
+  validateToken,
+  checkNameProduct,
+  checkAmountProduct,
+  createProduct,
+);
+app.use(errorMidleware);
 
 export default app;
