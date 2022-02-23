e708f9b0ab0b239f42cfb0d7c3b39855e4f3cb6c
Julio <julioaranabr@yahoo.com.br>
2022-02-23 18:46:38 -0300

    refatored validate token

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -2,16 +2,27 @@ import { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { Secret, verify } from 'jsonwebtoken';
 
+export interface PersonalRequest extends Request {
+  user?: number;
+}
+interface Token {
+  id: number,
+  userName: string,
+  iat: number,
+}
+
 dotenv.config();
 const key: Secret = process.env.JWT_SECRET || 'julio';
 
-const validateToken = (req: Request, res: Response, next: NextFunction) => {
+const validateToken = (req: PersonalRequest, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
   if (!authorization) {
     return res.status(401).json({ error: 'Token not found' });
   }
   try {
-    verify(authorization, key);
+    const decode = verify(authorization, key);
+    const token = decode as Token;
+    req.user = token.id;
     next();
   } catch (error) {
     res.status(401).json({ error: 'Invalid token' });
