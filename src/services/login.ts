e4801003d0a82a7c355ab8e19b7ef57567097584
Julio <julioaranabr@yahoo.com.br>
2022-02-22 21:54:50 -0300

    login service

---
+++ src/services/login.ts
@@ -0,0 +1,18 @@
+import { getUserByName, NewUser } from '../models/user';
+
+export type ErrorMessage = {
+  code: number;
+  message:{ error: string }
+};
+
+const loginUser = async (obj: NewUser) => {
+  const user = await getUserByName(obj);
+  const message: ErrorMessage = { code: 401, message: { error: 'Username or password invalid' } };
+  if (!user) {
+    return message;
+  } 
+  
+  return user;
+};
+
+export default loginUser;
