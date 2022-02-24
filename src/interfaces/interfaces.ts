6b63afaa5dc9d59076febd55eabd4926740f3c18
Julio <julioaranabr@yahoo.com.br>
2022-02-24 17:42:20 -0300

    created folder interfaces

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,25 @@
+export interface User extends NewUser {
+  id: number
+}
+export interface NewUser {
+  username: string;
+  classe: string;
+  level: number;
+  password: string;
+}
+
+export interface Product extends NewProduct {
+  id: number;
+  orderId: number | null;
+}
+
+export interface NewProduct {
+  name: string;
+  amount: string;
+}
+
+export interface Order {
+  id: number;
+  productId: number;
+  userId: number;
+}
