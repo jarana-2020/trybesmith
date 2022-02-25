5f0cc8facc5af41b0b2b9580089cc84ab0e27406
Julio <julioaranabr@yahoo.com.br>
2022-02-24 23:03:01 -0300

    refatored order controller

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,30 +1,47 @@
 import { Request, Response } from 'express';
+import { OrderProducts } from '../interfaces/interfaces';
 import { PersonalRequest } from '../middlewares/validateToken';
 import { ErrorMessage } from '../services/login';
 import createOrder, { allOrders, getOrderById } from '../services/order';
+import { OrderCreated, ReceivedOrder } from '../types/product';
 
-const createNewOrder = async (req: PersonalRequest, res: Response) => {
-  const { products } = req.body;
-  const id = req.user as number;
-  const order = await createOrder({ user: id, products });
-  res.status(201).json(order);
-};
-
-export const orderById = async (req: PersonalRequest, res: Response) => {
-  const { id } = req.params;
-  const orderId = Number(id);
-  const userId = req.user as number;
-  const order = await getOrderById(userId, orderId);
-  const orderMessage = order as ErrorMessage;
-  if (orderMessage.message) {
-    return res.status(orderMessage.code).json(orderMessage.message);
+const createNewOrder = async (req: PersonalRequest, res: Response):
+Promise<Response<OrderCreated | Error>> => {
+  try {
+    const { products } = req.body as OrderProducts;
+    const id = req.user as number;
+    const order = await createOrder({ user: id, products });
+    return res.status(201).json(order);
+  } catch (error) {
+    return res.status(500).json(error);
   }
-  return res.status(200).json(order);
 };
 
-export const getAllOrders = async (req: Request, res: Response) => {
-  const orders = await allOrders();
-  return res.status(200).json(orders);
+export const orderById = async (req: PersonalRequest, res: Response):
+Promise<Response<ReceivedOrder | Error>> => {
+  try {
+    const { id } = req.params;
+    const orderId = Number(id);
+    const data = await getOrderById(orderId);
+    const order = data as ReceivedOrder;
+    const orderMessage = data as ErrorMessage;
+    if (orderMessage.message) {
+      return res.status(orderMessage.code).json(orderMessage.message);
+    }
+    return res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const getAllOrders = async (_req: Request, res: Response)
+:Promise<Response<ReceivedOrder[] | Error>> => {
+  try {
+    const orders = await allOrders();
+    return res.status(200).json(orders);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 export default createNewOrder;
