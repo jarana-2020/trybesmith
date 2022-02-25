eac52e35a91d540d7539484d0a2d2be27e748897
Julio <julioaranabr@yahoo.com.br>
2022-02-24 23:03:16 -0300

    refatored order model

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,31 +1,32 @@
-import { FieldPacket, OkPacket } from 'mysql2';
+import { OkPacket, RowDataPacket } from 'mysql2';
 import { Order } from '../interfaces/interfaces';
 import connection from './connection';
 
-const insertOrder = async (userId: number) => {
+const insertOrder = async (userId: number):Promise<{ id: number }> => {
   const query = 'INSERT INTO Trybesmith.Orders (userId) VALUE(?)';
-  const [order]:[OkPacket, FieldPacket[]] = await connection.execute(query, [userId]);
+  const [order] = await connection.execute<OkPacket>(query, [userId]);
   return {
     id: order.insertId,
   };
 };
 
-export const orderById = async (id: number) => {
-  const query = `SELECT P.id as productId 
+export const orderById = async (id: number):Promise<Order[]> => {
+  const query = `
+    SELECT P.id as productId, O.userId 
     FROM Trybesmith.Orders as O
     INNER JOIN Trybesmith.Products as P
     ON O.id = P.orderId
     WHERE O.id = ?`;
-  const [result] = await connection.execute(query, [id]);
+  const [result] = await connection.execute<RowDataPacket[]>(query, [id]);
   const order = result as Order[];
   return order;
 };
 
-export const getAllOrders = async () => {
+export const getAllOrders = async ():Promise<Order[]> => {
   const query = `
     SELECT *
     FROM Trybesmith.Orders`;
-  const [result] = await connection.execute(query);
+  const [result] = await connection.execute<RowDataPacket[]>(query);
   const order = result as Order[];
   return order;
 };
