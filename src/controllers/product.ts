03ef2d5359cc13ed96111ef17df484bee2bfa565
Julio <julioaranabr@yahoo.com.br>
2022-02-25 09:09:33 -0300

    refatored product controller

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,22 +1,26 @@
 import { Request, Response } from 'express';
+import { Product } from '../interfaces/interfaces';
 import { PersonalRequest } from '../middlewares/validateToken';
 import createNewProduct, { allProducts } from '../services/product';
+import { ProductCreated } from '../types/product';
 
-const createProduct = async (req: PersonalRequest, res: Response) => {
+const createProduct = async (req: PersonalRequest, res: Response):
+Promise<Response<ProductCreated | Error>> => {
   try {
     const product = await createNewProduct(req.body);
     return res.status(201).json(product);
   } catch (error) {
-    res.status(500).json(error);
+    return res.status(500).json(error);
   }
 };
 
-export const getAllProducts = async (_req: Request, res: Response) => {
+export const getAllProducts = async (_req: Request, res: Response):
+Promise<Response<Product[] | Error>> => {
   try {
     const products = await allProducts();
     return res.status(200).json(products);
   } catch (error) {
-    res.status(500).json(error);
+    return res.status(500).json(error);
   }
 };
 
